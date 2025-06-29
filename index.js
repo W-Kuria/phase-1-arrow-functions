e97a5204a37942a72f7c32d63ec87ff5a785fb0a
W-Kuria <william.njoroge.kuria@gmail.com>
2025-06-29 14:34:30 +0300

    complete

---
+++ index.js
@@ -0,0 +1,31 @@
+const foo = function() {
+  return 'bar';
+};
+
+const boo = () => 'bar';
+
+const add = (a, b) => a + b;
+
+const sayHello = name => `Hello, ${name}!`;
+
+const square = x => x * x;
+
+const multiply = (a, b) => {
+  console.log(`Multiplying ${a} by ${b}`);
+  return a * b;
+};
+
+const no = [1, 2, 3];
+const sq = numbers.map(n => n * n);
+
+const squar = n => n * n;
+const numbers = [1, 2, 3];
+const squares = numbers.map(square);
+
+ function divide() {
+return 2000/100;
+}
+    
+
+
+
